a11fa0c66cc0ea0ce2557fa8bade56dfdb584aa5
Willian Eduardo <wieduardo13@hotmail.com>
2023-04-21 16:56:47 -0300

    Adicionado modo responsivo ao footer

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -17,6 +17,12 @@ export const MediaContainer= styled.div`
     display:flex;
     flex-direction: row;
     justify-content: center;
+
+    @media(max-width: 800px){
+        flex-direction:column;
+        align-items: center;
+        gap: 20px;
+    }
 `
 export const FooterMedia = styled.ul`
     width: 40vw;
@@ -59,6 +65,12 @@ export const FooterMedia = styled.ul`
           color: #4267B2;
         }
     }
+
+    @media(max-width: 800px){
+        border-right: none;
+        width: 90vw;
+    }
+
 `
 
 export const FooterEndereco = styled.div`
@@ -69,4 +81,9 @@ export const FooterEndereco = styled.div`
     align-items: center;
     justify-content: center;
     gap:5px;
+
+    @media(max-width: 800px){
+        width: 90vw;
+        min-height:10vh;
+    }
 `
